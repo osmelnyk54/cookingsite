d5bc17fa23e1380efda879e7a1d96280cf40567e
osmelnyk54 <70626806+osmelnyk54@users.noreply.github.com>
2020-12-07 16:43:42 +0200

    Update urls.py
    ..

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^recepts.html', views.post_list_recept_day, name='post_list_recept_day'),
-    
+    url(r'^holiday.html', views.post_list_holiday, name='post_list_holiday'),
+    url(r'^health.html', views.post_list_health, name='post_list_health'),
+    url(r'^modelss.html', views.post_list_modelss, name='post_list_modelss'),
+    url(r'^equipment.html', views.post_list_equipment, name='post_list_equipment'),
+    url(r'^thematic.html', views.post_list_thematic, name='post_list_thematic'),
 ]
