005ea6335add7bbb16d6365f342b62603412eb93
osmelnyk54 <70626806+osmelnyk54@users.noreply.github.com>
2020-12-07 17:10:14 +0200

    Update urls.py
    ..

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.post_list_main, name='post_list_main'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^recepts.html', views.post_list_recept_day, name='post_list_recept_day'),
+    url(r'^recepts.html', views.post_list_day, name='post_list_day'),
     url(r'^holiday.html', views.post_list_holiday, name='post_list_holiday'),
     url(r'^health.html', views.post_list_health, name='post_list_health'),
     url(r'^modelss.html', views.post_list_modelss, name='post_list_modelss'),
